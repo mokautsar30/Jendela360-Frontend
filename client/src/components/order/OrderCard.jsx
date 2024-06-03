8d84d89a49709841ecfde5e147338d3fbdcebea5
mokautsar30 <mohammadkautsar30@gmail.com>
2024-06-03 16:35:35 +0700

    feat: order-card-prototype

--- client/src/components/order/OrderCard.jsx
+++ client/src/components/order/OrderCard.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const OrderCard = () => {
   return (
-    <div>OrderCard</div>
+    <>
+      <div className='bg-white p-4 m-2 rounded-lg shadow-lg'>
+        <h3>
+          <p>name pemesan</p>
+          <p>sumber order</p>
+          <p>email</p>
+          <p>no hp</p>
+          <p>jumlah</p>
+          <p>keterangan</p>
+        </h3>
+      </div>
+    </>
   )
 }
 
