34757347017e168037eabb90b074d27976ff2042
mokautsar30 <mohammadkautsar30@gmail.com>
2024-06-03 15:20:55 +0700

    feat: boiler-plate

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-col bg-black justify-center items-center'>
+      <h1 className='font-bold text-white text-2xl'>hello jendela360</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
